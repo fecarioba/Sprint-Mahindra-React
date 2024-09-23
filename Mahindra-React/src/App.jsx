793459d1e16f67101a5548d34fdaba9f5aa558bf
fecarioba <feecarioba@gmail.com>
2024-09-22 23:46:09 -0300

    Update App.jsx

--- Mahindra-React/src/App.jsx
+++ Mahindra-React/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header.jsx";
 import Slider from "./components/Slider/Slider.jsx";
 import React from "react";
+import "../src/index.css";
 // desestruturar os hooks depois
 
 function App() {
