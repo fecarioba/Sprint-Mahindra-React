6910e3d66d978c834335afb8ba6536598b22e7bb
Rafael Oliveira <yraffa.olive@gmail.com>
2024-09-25 08:44:40 -0300

    update

--- Mahindra-React/src/App.jsx
+++ Mahindra-React/src/App.jsx
@@ -1,8 +1,6 @@
 import Header from "./components/Header/Header.jsx";
 import Slider from "./components/Slider/Slider.jsx";
 import Home from "./components/Home/Home.jsx";
-import Faq from "./components/Faq/Faq.jsx";
-import Login from "./components/Login/Login.jsx";
 import React from "react";
 import "../src/index.css";
 
@@ -11,9 +9,7 @@ function App() {
     <>
       <Header />
       <Home/>
-      <Slider />
-      <Faq/> 
-      <Login/>
+      <Slider/>
     </>
   );
 }
