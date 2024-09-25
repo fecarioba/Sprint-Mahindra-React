39e0a2251cea7261fc601f2336516794a011b75a
Felipe <162995713+fecarioba@users.noreply.github.com>
2024-09-25 08:43:26 -0300

    correcao slider

--- Mahindra-React/src/App.jsx
+++ Mahindra-React/src/App.jsx
@@ -1,10 +1,8 @@
 import Header from "./components/Header/Header.jsx";
 import Slider from "./components/Slider/Slider.jsx";
 import Home from "./components/Home/Home.jsx";
-import Faq from "./components/Faq/Faq.jsx";
-import Login from "./components/Login/Login.jsx";
-import React from "react";
 import "../src/index.css";
+// importar o faq e login depois
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <Header />
       <Home/>
       <Slider />
-      <Faq/> 
-      <Login/>
     </>
   );
 }
