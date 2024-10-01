96886606d8d1b647dc46498d61bdf1f4b7f2adad
fecarioba <feecarioba@gmail.com>
2024-09-30 22:02:44 -0300

    configuracao pages

--- Mahindra-React/vite.config.js
+++ Mahindra-React/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sprint-Mahindra-React'
+  base: '/Sprint-Mahindra-React',
 })
